713952a29ab7612640acfa419263c3caabd6b679
Steven van der Baan <steven@vdbaan.net>
2017-10-11 12:30:41 +0100

    Travis-CI Headless test
    Signed-off-by: Steven van der Baan <steven@vdbaan.net>

--- src/test/groovy/net/vdbaan/issuefinder/util/TableUtilsTest.groovy
+++ src/test/groovy/net/vdbaan/issuefinder/util/TableUtilsTest.groovy
@@ -9,27 +9,27 @@ import javafx.scene.input.KeyEvent
 import org.junit.Test
 import org.junit.runner.RunWith
 
-//@RunWith(JfxRunner.class)
+@RunWith(JfxRunner.class)
 class TableUtilsTest {
 
     @Test
-//    @TestInJfxThread
+    @TestInJfxThread
     void testUtilsWrongKeys() {
-//        TableView view = new TableView()
-//        TableUtils.installCopyPasteHandler(view)
-//        KeyEvent key = new KeyEvent(KeyEvent.KEY_PRESSED, "a", "",
-//                KeyCode.UNDEFINED, false, false, false, false)
-//        view.fireEvent(key)
+        TableView view = new TableView()
+        TableUtils.installCopyPasteHandler(view)
+        KeyEvent key = new KeyEvent(KeyEvent.KEY_PRESSED, "a", "",
+                KeyCode.UNDEFINED, false, false, false, false)
+        view.fireEvent(key)
     }
 
     @Test
-//    @TestInJfxThread
+    @TestInJfxThread
     void testUtilsRightKeys() {
-//        TableView view = new TableView()
-//        TableUtils.installCopyPasteHandler(view)
-//        KeyEvent key = new KeyEvent(KeyEvent.KEY_PRESSED,"ctrl-C", "",
-//                KeyCode.C, false, true, false, false)
-//        key.source = view
-//        view.getOnKeyPressed().handle(key)
+        TableView view = new TableView()
+        TableUtils.installCopyPasteHandler(view)
+        KeyEvent key = new KeyEvent(KeyEvent.KEY_PRESSED,"ctrl-C", "",
+                KeyCode.C, false, true, false, false)
+        key.source = view
+        view.getOnKeyPressed().handle(key)
     }
 }
